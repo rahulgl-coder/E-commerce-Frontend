9f45b64467e1a301be8c7c4b9f64c516953bd43f
rahulgl-coder <rahulnandhu0521@gmail>
2025-06-30 15:00:50 +0530

    api changed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,30 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: parseInt(process.env.PORT) || 5173,
+//     allowedHosts: 'all' // ✅ Let it access any external host
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 5173,
-    allowedHosts: 'all' // ✅ Let it access any external host
+    allowedHosts: [
+      'e-commerce-frontend-1-7tv7.onrender.com', // ✅ Allow this host
+      'localhost' // ✅ Also allow localhost for dev
+    ]
   }
 })
+
